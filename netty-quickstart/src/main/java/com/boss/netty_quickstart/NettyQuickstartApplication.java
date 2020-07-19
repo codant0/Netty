9b389e8f39f3f038c7dfaa3bcbeabe6efc11dd3c
code-zebra <879128177@qq.com>
2020-07-19 14:07:55 +0800

    Netty Quickstart Project for Spring Boot

---
+++ netty-quickstart/src/main/java/com/boss/netty_quickstart/NettyQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.boss.netty_quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyQuickstartApplication.class, args);
+    }
+
+}
---
+++ netty-quickstart/src/test/java/com/boss/netty_quickstart/NettyQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boss.netty_quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
